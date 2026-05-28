a51243cb632478ac104c92de9c48c563b9cdc4bb
Alexei Tchervinsky <alexei@tchervinsky.ru>
2026-05-29 00:27:50 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::cout << "Group  5130904/50003 emails << '\n';
+
+  std::string emails[] {
+  // добавлять корпоративные email здесь
+}
+
+for (const auto & e: emails)
+  {
+    std::cout << e << '\n`;
+  }
+
+  return 0;
+}

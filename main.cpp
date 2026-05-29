ec53cce4ae6fa1ee480564c8f40f7f610aeb773b
Ayjan Tegelekova <ajdzantegelekova19@gmail.com>
2026-05-29 17:01:19 +0300

    tegelekova.a

--- main.cpp
+++ main.cpp
@@ -7,18 +7,7 @@ int main()
 
   std::string emails[] {
   // добавлять корпоративные email здесь
-  "altynbaeva.af@edu.spbstu.ru",
-  "usmanov.di@edu.spbstu.ru",
-  "fomina.vk@edu.spbstu.ru",
-  "volkov.av@spbstu.ru",
-  "malevanaya.ms@edu.spbstu.ru",
-  "yuriev.dl@edu.spbstu.ru",
-  "koropov.vo@edu.spbstu.ru",
-  "chervinskij_ap@spbstu.ru",
-  "tsuremichev.ga@edu.spbstu.ru",
-  "tomilova2.em@edu.spbstu.ru",
-  "barzhin.yaa@edu.spbstu.ru",
-  "rtitshev.gs@edu.spbstu.ru",
+ tegelekova.a@edu.spbstu.ru
 };
 
   for (const auto & e : emails)

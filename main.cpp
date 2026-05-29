e779723e8226ef4bb002761a46d1e933f90adf9c
kabishev.daniil <sooodo4kaluuuv10010@gmail.com>
2026-05-29 17:11:24 +0300

    added brench

--- main.cpp
+++ main.cpp
@@ -7,19 +7,7 @@ int main()
 
   std::string emails[] {
   // добавлять корпоративные email здесь
-  "kabishev.da@edu.spbstu.ru",
-  "altynbaeva.af@edu.spbstu.ru",
-  "usmanov.di@edu.spbstu.ru",
-  "fomina.vk@edu.spbstu.ru",
-  "volkov.av@spbstu.ru",
-  "malevanaya.ms@edu.spbstu.ru",
-  "yuriev.dl@edu.spbstu.ru",
-  "koropov.vo@edu.spbstu.ru",
-  "chervinskij_ap@spbstu.ru",
-  "tsuremichev.ga@edu.spbstu.ru",
-  "tomilova2.em@edu.spbstu.ru",
-  "barzhin.yaa@edu.spbstu.ru",
-  "rtitshev.gs@edu.spbstu.ru",
+  "kabishev.da@edu.spbstu.ru"
 };
 
   for (const auto & e : emails)

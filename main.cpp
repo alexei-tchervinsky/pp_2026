f80b0919e8d2c2c62ffacea3fb2c8533b8317a66
Olya Rizleyzeva <olyarizleyzeva@MacBook-Air-Olya.local>
2026-05-29 14:40:11 +0300

    ryzlejtseva.oe

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
   std::cout << "Group  5130904/50003 emails" << '\n';
 
   std::string emails[] {
-  // добавлять корпоративные email здесь
+  //ryzlejtseva.oe@edu.spbstu.ru
 };
 
 for (const auto & e: emails)

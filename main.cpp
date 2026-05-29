769e98c93c9845fe9f5ba3453125250402e0e277
egorova.darina <motonikerdragan@gmail.com>
2026-05-29 16:55:29 +0300

    egorova.darina

--- main.cpp
+++ main.cpp
@@ -6,25 +6,14 @@ int main()
   std::cout << "Group  5130904/50003 emails" << '\n';
 
   std::string emails[] {
-  // добавлять корпоративные email здесь
-  "altynbaeva.af@edu.spbstu.ru",
-  "usmanov.di@edu.spbstu.ru",
-  "fomina.vk@edu.spbstu.ru",
-  "volkov.av@spbstu.ru",
-  "malevanaya.ms@edu.spbstu.ru",
-  "yuriev.dl@edu.spbstu.ru",
-  "koropov.vo@edu.spbstu.ru",
-  "chervinskij_ap@spbstu.ru",
-  "tsuremichev.ga@edu.spbstu.ru",
-  "tomilova2.em@edu.spbstu.ru",
-  "barzhin.yaa@edu.spbstu.ru",
-  "rtitshev.gs@edu.spbstu.ru",
+  "egorova.di@edu.spbstu.ru"
 };
 
-  for (const auto & e : emails)
+for (const auto & e: emails)
   {
     std::cout << e << '\n';
   }
 
   return 0;
 }
+

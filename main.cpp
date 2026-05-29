ff09a2f1577f306efc9cbd485be40adbaf16732c
kabishev.daniil <sooodo4kaluuuv10010@gmail.com>
2026-05-29 14:52:40 +0300

    grim reaper

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 
   std::string emails[]{
       // добавлять корпоративные email здесь
-      "tsuremichev.ga@edu.spbstu.ru"};
+      "kabishev.da@edu.spbstu.ru"};
 
   for (const auto &e : emails)
   {
